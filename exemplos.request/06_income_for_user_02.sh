04f94d5ce085579152dbfcbd23399b3d8870a568
Matheus Sarno Muiños <matheusmuinos@gmail.com>
2025-05-11 00:58:34 +0000

    feat: requisições income e expense funcionais

--- exemplos.request/06_income_for_user_02.sh
+++ exemplos.request/06_income_for_user_02.sh
@@ -1,8 +1,8 @@
 curl -X POST http://localhost:3000/transactions \
 -H "Content-Type: application/json" \
--H "Authorization: Bearer $USER2_TOKEN" \
 -d '{
-    "amount": 1500.00,
-    "description": "Freelance",
-    "type": "income"
+    "amount": 3000.00,
+    "description": "Salário",
+    "type": "income",
+    "userId": 2
 }'
